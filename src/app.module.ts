f2616c1ab5e2f4162a7ccf49b7167fb1d5e0de76
insideyiu <a22311061@uthermosillo.edu.mx>
2023-10-07 12:24:54 -0700

    Conectarse a mongo mediante varaibles de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from './auth/auth.module';
+
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017'), // Conexion a mongo
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI), // Conexion a mongo mediante variable de entorno
     AuthModule,
   ],
   controllers: [],
