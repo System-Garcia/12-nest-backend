d2331b6798ff6c709d7c1f76ce397f05df57cad5
insideyiu <a22311061@uthermosillo.edu.mx>
2023-10-07 12:52:44 -0700

    Crear esquema de usuario

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class User {
+  // _id: string // Mongo lo crea automaticamente
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ unique: true, required: true })
+  email: string;
+
+  @Prop({ minlength: 6, required: true})
+  password: string;
+
+  @Prop({ default: true }) // Opcion por defecto
+  isActive: boolean;
+
+  @Prop({ type: [String], default: ['user']}) // El type con String en mayuscula es para decirle a la base de datos el tipo
+  roles: string[];
+}
+
+
+export const UserSchema = SchemaFactory.createForClass( User ); // Creamos el esquema usando la clase
