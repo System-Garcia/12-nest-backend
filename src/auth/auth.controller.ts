28043b97ec6b0b16eb4e2b33e3450011230c9beb
insideyiu <a22311061@uthermosillo.edu.mx>
2023-10-13 14:16:06 -0700

    Habilitar acceso desde distintos origines

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request }
 import { AuthService } from './auth.service';
 import {CreateUserDto, LoginDto, RegisterUserDto, UpdateAuthDto} from './dto'
 import { AuthGuard } from './guards/auth.guard';
+import { User } from './entities/user.entity';
+import { LoginResponse } from './interfaces/login-response';
 
 @Controller('auth')
 export class AuthController {
@@ -22,24 +24,37 @@ export class AuthController {
     return this.authService.register(registerUserDto);
   }
 
-  @UseGuards( AuthGuard )
+  @UseGuards(AuthGuard)
   @Get()
-  findAll( @Request() req: Request) {
+  findAll(@Request() req: Request) {
     return this.authService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+  @UseGuards(AuthGuard)
+  @Get('check-token')
+  checkToken( @Request() req: Request ): LoginResponse {
+    
+    const user = req['user'] as User;
+    console.log(user)
+    return {
+      user,
+      token: this.authService.getJwtToken({ id: user._id})
+    }
+
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.authService.findOne(+id);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+  //   return this.authService.update(+id, updateAuthDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.authService.remove(+id);
+  // }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes( // Obligar a que la informacion venga como quiero o se rechaza
+  app.enableCors(); // Habilitar que accesible desde orígenes diferentes al de tu aplicación
+
+  app.useGlobalPipes(
+    // Obligar a que la informacion venga como quiero o se rechaza
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
